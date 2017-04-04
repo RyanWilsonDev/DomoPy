6f77cc2a5a1a1c0a448d5a02d36ee41285d8fed9
Ryan Wilson <11.ryan.wilson@gmail.com>
2017-04-04 12:26:37 -0700

    Management API Skeletons

--- domopy/groupmanagement.py
+++ domopy/groupmanagement.py
@@ -18,3 +18,62 @@ Delete
 Remove
     Remove a user from a group
 """
+
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib.oauth2_session import OAuth2Session
+
+
+class DomoGroups:
+    """Facade for Domo Group API."""
+
+    _client = None
+    _domo = None
+    _token = None
+
+    def __init__(self, clientId, clientSecret):
+        """Configure Backend Client."""
+        self._set_client(client_id=clientId)
+        self._get_token(client_id=clientId, client_secret=clientSecret)
+
+    def _set_client(self, client_id):
+        """Create backend client for Domo API connection."""
+        self._client = BackendApplicationClient(client_id=client_id)
+        self._domo = OAuth2Session(client=self._client)
+
+    def _get_token(self, client_id, client_secret):
+        """Authenticate with Domo API and retrieve Auth Token."""
+        token_url = 'https://api.domo.com/oauth/token?grant_type=client_credentials&scope=users'
+        self._token = self._domo.fetch_token(
+            token_url=token_url, client_id=client_id, client_secret=client_secret)
+
+    def create_group(self):
+        """Create new Domo User Group."""
+        pass
+
+    def add_user(self, user_id, group_id):
+        """Add a user to a given User Group."""
+        pass
+
+    def remove_user(self, user_id, group_id):
+        """Remove a specified user from a given User Group."""
+        pass
+
+    def list_groups(self):
+        """List Domo User Groups in Domo instance."""
+        pass
+
+    def list_group_users(self, group_id):
+        """Get a Domo Users in a group."""
+        pass
+
+    def get_group(self, group_id):
+        """Get info for a user group."""
+        pass
+
+    def update_group(self, group_id):
+        """Update a Domo User Group."""
+        pass
+
+    def delete_user(self, group_id):
+        """Delete a Domo User Group by Id."""
+        pass
--- domopy/usermanagement.py
+++ domopy/usermanagement.py
@@ -1,3 +1,4 @@
+
 """
 
 User APIs
@@ -13,3 +14,49 @@ Update
 Delete
     Delete a User
 """
+
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib.oauth2_session import OAuth2Session
+
+class DomoUsers:
+    """Facade for Domo Users API."""
+
+    _client = None
+    _domo = None
+    _token = None
+
+    def __init__(self, clientId, clientSecret):
+        """Configure Backend Client."""
+        self._set_client(client_id=clientId)
+        self._get_token(client_id=clientId, client_secret=clientSecret)
+
+    def _set_client(self, client_id):
+        """Create backend client for Domo API connection."""
+        self._client = BackendApplicationClient(client_id=client_id)
+        self._domo = OAuth2Session(client=self._client)
+
+    def _get_token(self, client_id, client_secret):
+        """Authenticate with Domo API and retrieve Auth Token."""
+        token_url = 'https://api.domo.com/oauth/token?grant_type=client_credentials&scope=users'
+        self._token = self._domo.fetch_token(
+            token_url=token_url, client_id=client_id, client_secret=client_secret)
+
+    def create_user(self):
+        """Create new Domo User."""
+        pass
+
+    def list_users(self):
+        """List Domo Users in Domo instance."""
+        pass
+
+    def get_user(self, user_id):
+        """Get a Domo User by Id."""
+        pass
+
+    def update_user(self, user_id):
+        """Update a Domo User."""
+        pass
+
+    def delete_user(self, user_id):
+        """Delete a Domo User by Id."""
+        pass
